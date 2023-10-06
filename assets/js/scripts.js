4c4d1d3b92af403cf12c64915bf89272d524cead
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-10-06 20:47:25 +0330

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+// swiper
+var banner = new Swiper(".banner", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
