c130ba236a74b429f4a5b091fc43cde236fbec64
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-10-07 16:22:17 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,7 +17,7 @@ overlay.addEventListener('click',function () {
 var banner = new Swiper(".banner", {
     loop: true,
     slidesPerView: 1,
-    spaceBetween: 20,
+    spaceBetween: 0,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
