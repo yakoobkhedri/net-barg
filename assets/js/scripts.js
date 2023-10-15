a24d8118eeba33634012cd866797ffc0480108c1
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-10-15 11:18:41 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -81,16 +81,3 @@ var tabs = new Swiper(".tabs", {
 });
 // aos
 AOS.init();
-
-// tabs
-
-var portfolio = $('#portfolio-container').isotope({
-    originLeft: false
-});
-$('#portfolio-filter > div').on('click', function () {
-    $("#portfolio-filter > div").removeClass('active');
-    $(this).addClass('active');
-    portfolio.isotope({
-        filter: $(this).data('filter')
-    });
-});
